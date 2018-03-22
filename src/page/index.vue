a5df3963ed938226db9c1e5f03306d28660ddf6b
liuyunhe <lyhdyxd@vip.qq.com>
2018-03-22 10:29:24 +0800

    增加异步请求异常的备用数据

--- src/page/index.vue
+++ src/page/index.vue
@@ -172,6 +172,15 @@
         doSomethingOnSlideShow(index) {
 //          console.log('doSomethingOnSlideShow' + index)
         }
+      },
+      mounted() {
+        if(this.newsList.length == 0){
+          this.newsList =[
+            {"id":1,"title":"新闻条目1新闻条目1新闻条目1新闻条目1","url":"http://starcraft.com"},
+            {"id":2,"title":"新闻条目2新闻条目2新闻条目2新闻条目2","url":"http://warcraft.com"},
+            {"id":3,"title":"新闻条3新闻条3新闻条3","url":"http://overwatch.com"},
+            {"id":4,"title":"新闻条4广告发布","url":"http://hearstone.com"}]
+        }
       }
     }
 </script>
