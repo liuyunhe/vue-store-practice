cd99b797b6d8fb9f162f25938f166f90da23c7d7
liuyunhe <lyhdyxd@vip.qq.com>
2018-04-12 14:01:22 +0800

    修改路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import IndexPage from '../page/index.vue'
 import Detail from '../page/detail.vue'
-import Forecast from  '../page/detail/forecast.vue'
-import Analysis from  '../page/detail/analysis.vue'
-import Count from  '../page/detail/count.vue'
-import Publish from  '../page/detail/publish.vue'
+import Sc2 from '../page/detail/sc2.vue'
+import D3 from '../page/detail/d3.vue'
+import Wow from '../page/detail/wow.vue'
+import Hs from '../page/detail/hs.vue'
 import OrderList from '../page/orderList.vue'
 import DatePicker from '../page/datePicker.vue'
 import User from '../page/user.vue'
@@ -33,21 +33,21 @@ export default new Router({
       redirect:'detail/count',
       component: Detail,
       children:[
-        { path: 'forecast',
-          name: 'Forecast',
-          component: Forecast
+        { path: 'sc2',
+          name: 'Sc2',
+          component: Sc2
         },
-        { path: 'analysis',
-          name: 'Analysis',
-          component: Analysis
+        { path: 'd3',
+          name: 'D3',
+          component: D3
         },
-        { path: 'count',
-          name: 'Count',
-          component: Count
+        { path: 'wow',
+          name: 'Wow',
+          component: Wow
         },
-        { path: 'publish',
-          name: 'Publish',
-          component: Publish
+        { path: 'hs',
+          name: 'HS',
+          component: Hs
         }
       ]
     },
